ebd454c40e37dc2672dbe49cbed3d2a088122249
gauravraoj <96016132+gauravraoj@users.noreply.github.com>
2023-08-09 11:12:37 +0530

    Create LibraryDAO.java

---
+++ LibraryDAO.java
@@ -0,0 +1,56 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LibraryDAO {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/library";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "your_password";
+
+    public List<Book> getAllBooks() {
+        List<Book> books = new ArrayList<>();
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
+            String query = "SELECT * FROM books";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String title = resultSet.getString("title");
+                boolean isAvailable = resultSet.getBoolean("is_available");
+                books.add(new Book(id, title, isAvailable));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return books;
+    }
+
+    public boolean issueBook(int bookId) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
+            String query = "UPDATE books SET is_available = false WHERE id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, bookId);
+            int rowsUpdated = preparedStatement.executeUpdate();
+            return rowsUpdated > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean returnBook(int bookId) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
+            String query = "UPDATE books SET is_available = true WHERE id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, bookId);
+            int rowsUpdated = preparedStatement.executeUpdate();
+            return rowsUpdated > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
